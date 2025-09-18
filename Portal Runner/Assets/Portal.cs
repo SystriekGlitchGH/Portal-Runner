32e50e6881b33c6e1ad72f8365df7de33e1856e8
SystriekGlitchGH <systriekglitchyt@gmail.com>
2025-09-18 08:51:35 -0700

    added working portals

---
+++ Portal Runner/Assets/Portal.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private HashSet<GameObject> portalObjects = new HashSet<GameObject>();
+
+    [SerializeField] private Transform destination;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (portalObjects.Contains(collision.gameObject))
+        {
+            return; // makes sure objects aren't constantly teleporting 
+        }
+        if (destination.TryGetComponent(out Portal destinationPortal))
+        {
+            destinationPortal.portalObjects.Add(collision.gameObject);
+        }
+
+        collision.transform.position = destination.position;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        portalObjects.Remove(collision.gameObject);
+    }
+}
