d8e1a12e44e7de6c930e0e0f68e4d78c2ad16b5f
SystriekGlitchGH <systriekglitchyt@gmail.com>
2025-10-20 09:47:35 -0700

    added comments to RoomPortal Script

--- Portal Runner/Assets/RoomPortal.cs
+++ Portal Runner/Assets/RoomPortal.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class RoomPortal : MonoBehaviour
 {
+    // makes a field in the scene editor to type in the name of the room you want the object to warp to
     [SerializeField] private string room;
-    private void OnTriggerEnter2D(Collider2D changeScene)
-    {
-        if (changeScene.gameObject.CompareTag("Player"))
+
+    /* when an object enters the collision box for the object,
+     * activate this method. collider is the object that is in the hitbox*/
+    private void OnTriggerEnter2D(Collider2D collider)
+    {   // if the collider has the tag player, load the scene with the String that the variable "name" has
+        if (collider.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene(room);
         }
